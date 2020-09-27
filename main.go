f9853db4a17a24c1beca79fa37eddcd12e16d92f
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-09-27 16:49:23 -0500

    prueba01

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func factorial(numero float64) float64 {
+	if numero == 0 {
+		return 1
+	}
+	return numero * factorial(numero-1)
+}
+
+func main() {
+	var x float64
+	for {
+		// fmt.Print("Indica el nímero límite para calcular el número E: ")
+		fmt.Scan(&x)
+		if x >= 0 {
+			break
+		}
+	}
+
+	var i float64
+	var resultado float64
+	for i = 0; i <= x; i++ {
+		resultado += 1 / factorial(i)
+	}
+
+	fmt.Println(resultado)
 }
